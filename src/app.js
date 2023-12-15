65f594b2dccf7002fc3d8b72fd413534a8b39e32
r00t <r00t@ragonsys.com>
2023-12-15 09:09:46 +0530

    Update CORS configuration in app.js

--- src/app.js
+++ src/app.js
@@ -40,8 +40,20 @@ const corsOptions = {
 };
 
 // enable cors
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+if (req.method == "OPTIONS") {
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  return res.status(200).json({});
+}
 
+next();
+});
 
 // app.options("*", cors());
 
