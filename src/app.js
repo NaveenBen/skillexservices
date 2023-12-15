80a60f3d30292763517d9240c824de947e386ecb
r00t <r00t@ragonsys.com>
2023-12-15 08:42:34 +0530

    Update JWT and CORS configuration

--- src/app.js
+++ src/app.js
@@ -30,7 +30,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 
 // enable cors
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: true,
+  optionsSuccessStatus: 204,
+}));
 app.options("*", cors());
 
 // jwt authentication
