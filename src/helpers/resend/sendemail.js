8dd185ebdbb8c59ad7b5051881108f8847ab622e
r00t <r00t@ragonsys.com>
2023-12-22 21:10:13 +0530

    Update Twilio auth token and email sender address

--- src/helpers/resend/sendemail.js
+++ src/helpers/resend/sendemail.js
@@ -6,8 +6,8 @@ const resend = new Resend(config.resend.apiKey);
 const sendEmail = async (email, subject, text) => {
   try {
   let x = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: "happydonorsngo@gmail.com",
+      from: 'no-reply@mail.happydonors.ngo',
+      to: email,
       subject,
       html: text,
       text,
@@ -15,6 +15,7 @@ const sendEmail = async (email, subject, text) => {
     return x;
   } catch (error) {
     console.log('🚀 ~ file: email.service.js:5 ~ sendEmail ~ error:', error);
+    throw error;
   }
 };
 
